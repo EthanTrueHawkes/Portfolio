2beea2fc98a2142d65afe624496e25a9dd544160
Ethan Hawkes <ehawkes3@byu.edu>
2026-02-08 19:42:33 -0700

    Updated Router to Route to Pages

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,66 @@
 import React from "react";
 import "./app.css";
 
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Router,
+  Routes,
+} from "react-router-dom";
+import { Login } from "./login/login";
+import { Resume } from "./resume/resume";
+import { About } from "./about/about";
+import { Home } from "./home/home";
+import { Notfound } from "./notfound/notfound";
+
 export default function App() {
   return (
-    <div className="app">
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="index.html">Projects</a>
-            </li>
-            <li>
-              <a href="resume.html">Resume</a>
-            </li>
-            <li>
-              <a href="about.html">About</a>
-            </li>
-          </ul>
+    <BrowserRouter>
+      <div className="app">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="">Projects</NavLink>
+              </li>
+              <li>
+                <NavLink to="resume">Resume</NavLink>
+              </li>
+              <li>
+                <NavLink to="about">About</NavLink>
+              </li>
+            </ul>
 
-          <a
-            href="login.html"
-            class="login-button"
-            title="Log in"
-            aria-label="Log in"
-          >
-            <img
-              src="Assets/Icons/Login-person-SVGicon.svg"
-              alt="Log In Icon"
-            />
+            <NavLink
+              to="login"
+              class="login-button"
+              title="Log in"
+              aria-label="Log in"
+            >
+              <img
+                src="Assets/Icons/Login-person-SVGicon.svg"
+                alt="Log In Icon"
+              />
+            </NavLink>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+
+        <footer>
+          <p>By Ethan Hawkes</p>
+          <a href="https://github.com/EthanTrueHawkes/Portfolio.git">
+            GitHub Repository
           </a>
-        </nav>
-      </header>
-
-      <footer>
-        <p>By Ethan Hawkes</p>
-        <a href="https://github.com/EthanTrueHawkes/Portfolio.git">
-          GitHub Repository
-        </a>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
