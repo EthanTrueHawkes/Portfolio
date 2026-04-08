9714e4e62912883977a047635911f2aa6763278a
Ethan Hawkes <ehawkes3@byu.edu>
2026-04-07 18:16:49 -0600

    Implemented heartbeat / ping pong

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -3,3 +3,12 @@ const { WebSocketServer, WebSocket } = require("ws");
 
 const app = express();
 app.use(express.static("./public"));
+
+setInterval(() => {
+  socketServer.clients.forEach(function each(client) {
+    if (client.isAlive === false) return client.terminate();
+
+    client.isAlive = false;
+    client.ping();
+  });
+}, 10000);
